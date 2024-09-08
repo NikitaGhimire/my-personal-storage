d50b99e4e4469a293fd0292fbe6f69e464c0af99
NikitaGhimire <nikitaghimire1@gmail.com>
2024-09-08 12:43:40 +1000

    Added babsic styling to all the views

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //middleware to parse JSON
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
